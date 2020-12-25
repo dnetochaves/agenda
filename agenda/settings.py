8bb80196f3e521699b43da9b67de984570bb80a7
dnetochaves <dnetochaves@gmail.com>
2020-12-25 10:04:26 -0300

    Customizando area administrativa django

--- agenda/settings.py
+++ agenda/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['3.95.243.228']
+ALLOWED_HOSTS = ['3.95.243.228', '127.0.0.1']
 
 
 # Application definition
